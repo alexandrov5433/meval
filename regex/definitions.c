547cea5b755dfa90b0e6d2501ebecd8beb19ba7a
alexandrov5433 <alexandrov5433@gmail.com>
2025-11-22 18:01:05 +0100

    String definitions for regex patterns added.

--- regex/definitions.c
+++ regex/definitions.c
@@ -2,3 +2,4 @@
 #define REGEX_RESERVED "[ep]+|ln|lg"
 #define REGEX_HELP "-h|--help"
 #define REGEX_VARIABLE "^(-v|--variable)=([A-Za-z]{1})=((-?[0-9]+)|(-?[0-9]+[\\.,]{1}[0-9]+))$"
+#define REGEX_EXP_PLACEHOLDER "^#([0-9]+)#$"
