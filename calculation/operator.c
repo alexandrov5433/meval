973ff1ea09c45e70c995047f41d422cb34243dd8
alexandrov5433 <alexandrov5433@gmail.com>
2025-11-23 11:14:04 +0100

    Operator logic update.

--- calculation/operator.c
+++ calculation/operator.c
@@ -3,12 +3,13 @@
 
 #include "array/_index.c"
 #include "operant.c"
+#include "expression.c"
 
 
-void calcOperator(CharArray *exp, int operatorIndex)
+void calcOperator(Expression *expression, VariableArray *variables, int operatorIndex)
 {
     double result = 0;
-    char operator = (exp->str)[operatorIndex];
+    char operator = (expression->expDummy->str)[operatorIndex];
 
     Operant leftOp = {
         .operantStr = newCharArray(NULL, 0),
@@ -21,11 +22,11 @@ void calcOperator(CharArray *exp, int operatorIndex)
 
     // left
     int indexLeft = operatorIndex - 1;
-    getLeftOperant(exp, &leftOp, &indexLeft);
+    getLeftOperant(expression, variables, &leftOp, &indexLeft);
 
     // right
     int indexRight = operatorIndex + 1;
-    getRightOperant(exp, &rightOp, &indexRight);
+    getRightOperant(expression, variables, &rightOp, &indexRight);
 
     // calculate rusult
     switch (operator)
@@ -53,7 +54,7 @@ void calcOperator(CharArray *exp, int operatorIndex)
     // clear evaluated characters; set their indexes to Space (32) ' '
     int clearStart = indexLeft + 1;
     int clearEnd = indexRight - 1;
-    clearSpace(exp, clearStart, clearEnd);
+    clearSpace(expression->expDummy, clearStart, clearEnd);
 
-    incertDoubleInStrAtIndex(exp, result, operatorIndex);
+    incertDoubleInStrAtIndex(expression->expDummy, result, operatorIndex);
 }
