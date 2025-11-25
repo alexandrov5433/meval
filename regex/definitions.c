ca5631b8fcbeca7ada78776c2f0288160292c1c4
alexandrov5433 <alexandrov5433@gmail.com>
2025-11-25 19:46:11 +0100

    Definition update.

--- regex/definitions.c
+++ regex/definitions.c
@@ -1,5 +1,5 @@
 #define REGEX_MATH_EXPRESSION "^(--expression|-e)='([ A-Za-z0-9(-/^-]+)'$"
 #define REGEX_RESERVED "[ep]+|ln|lg"
-#define REGEX_HELP "-h|--help"
+#define REGEX_HELP "^(-h|--help)$"
 #define REGEX_VARIABLE "^(-v|--variable)=([A-Za-z]{1})=((-?[0-9]+)|(-?[0-9]+[\\.,]{1}[0-9]+))$"
 #define REGEX_EXP_PLACEHOLDER "^#([0-9]+)#$"
