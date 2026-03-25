e7656bfa4fbae74263e16ce3ea4fb9eaa0ea4843
alexandrov5433 <alexandrov5433@gmail.com>
2026-03-25 21:24:00 +0100

    Galxlib integrated.

--- src/argumentParsing/variable.c
+++ src/argumentParsing/variable.c
@@ -3,21 +3,79 @@
 #include <string.h>
 
 #include "variable.h"
-#include "../array/_index.h"
 #include "../regex/_index.h"
+#include "galxlib/dynamic_array.h"
 
-void parseVariable(VariableArray *varArr, const char argc, char **argv) {
-    RegexContainer *container = newRegexContainer(REGEX_VARIABLE, 6, REG_EXTENDED);
-    // group indexes: 2, 3
-    for (int i = 1; i < argc; i++) {
-        match(argv[i], container);
-        if (container->isMatch == 1) {
-            char name = getLastChar(getGroupValueAsCharArray(2, container));
-            CharArray *charArr = getGroupValueAsCharArray(3, container);
-            double value = strtod(charArr->str, NULL);
-            Variable *variable = newVariable(name, value);
-            appendVariable(variable, varArr);
-        }
-    }
-    freeRegexContainer(container);
+/**
+ * Creates a new Varaible.
+ * @param name The value for the name property of the Variable.
+ * @param value The value for the value property of the Variable.
+ * @return The pointer to the newly created Variable.
+ */
+Variable *new_variable(const char name, const double value)
+{
+	Variable *variable = calloc(1, sizeof(Variable));
+	if (variable == NULL)
+		return NULL;
+
+	variable->name = name;
+	variable->value = value;
+	return variable;
+}
+
+/**
+ * Frees the memory for the Variable instance.
+ * @param var A pointer to the Variable to free. If NULL, nothing is done.
+ */
+inline void free_variable(Variable *var)
+{
+	if (var == NULL)
+		return;
+	free(var);
+}
+
+/**
+ * Parses the variables from argv.
+ * @returns 0 on success, 1 on failure.
+ */
+int parse_variable(DynamicArray *var_arr, const char argc, char **argv)
+{
+	// group indexes: 2, 3
+	RegexContainer *container = newRegexContainer(REGEX_VARIABLE, 6, REG_EXTENDED);
+
+	for (int i = 1; i < argc; i++)
+	{
+		match(argv[i], container);
+		if (container->isMatch == 1)
+		{
+			String *tmp = getGroupValueAsString(2, container);
+			char name = (tmp->str)[tmp->length - 1];
+			free_string(tmp);
+
+			String *str = getGroupValueAsString(3, container);
+			char *str_nt = NULL;
+			int err_str = get_raw_nt(str, &str_nt);
+			if (err_str != 0 || str_nt == NULL)
+			{
+				free_string(str);
+				return 1;
+			}
+			free_string(str);
+
+			double value = strtod(str_nt, NULL);
+
+			Variable *var = new_variable(name, value);
+			if (var == NULL)
+				return 1;
+
+			int err_da = push_da(var_arr, var);
+			if (err_da != 0)
+			{
+				free_variable(var);
+				return 1;
+			}
+		}
+	}
+	freeRegexContainer(container);
+	return 0;
 }
--- src/argumentParsing/variable.h
+++ src/argumentParsing/variable.h
@@ -1,10 +1,24 @@
-#include <stdlib.h>
-#include "../array/_index.h"
-#include "../regex/_index.h"
-
 #ifndef ARGUMENT_PARSING_VARIABLE
 #define ARGUMENT_PARSING_VARIABLE
 
-void parseVariable(VariableArray *varArr, const char argc, char **argv);
+#include <stdlib.h>
+#include "../regex/_index.h"
+#include "galxlib/dynamic_array.h"
+
+/**
+ * @param name The name of the Variable - a single character.
+ * @param value The value of the Variable - a double.
+ */
+typedef struct Variable
+{
+	char name;
+	double value;
+} Variable;
+
+Variable *new_variable(const char name, const double value);
+
+inline void free_variable(Variable *var);
+
+int parse_variable(DynamicArray *var_arr, const char argc, char **argv);
 
 #endif
